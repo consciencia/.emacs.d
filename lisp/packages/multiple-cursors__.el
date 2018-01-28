451fc068d33c17dc3b23715da0918618ce8ba8c3
box <box@localhost.localdomain>
2018-01-28 19:07:13 +0100

    comments removed

--- lisp/packages/multiple-cursors__.el
+++ lisp/packages/multiple-cursors__.el
@@ -2,10 +2,6 @@
 (require 'multiple-cursors)
 
 (defun custom/mc/mark-next-like-this (arg)
-  "Find and mark the next part of the buffer matching the currently active region
-If no region is active add a cursor on the next line
-With negative ARG, delete the last one instead.
-With zero ARG, skip the last one and mark next."
   (interactive "p")
   (if (< arg 0)
       (let ((cursor (mc/furthest-cursor-after-point)))
@@ -16,10 +12,6 @@ With zero ARG, skip the last one and mark next."
   (mc/maybe-multiple-cursors-mode))
 
 (defun custom/mc/mark-prev-like-this (arg)
-  "Find and mark the next part of the buffer matching the currently active region
-If no region is active add a cursor on the next line
-With negative ARG, delete the last one instead.
-With zero ARG, skip the last one and mark next."
   (interactive "p")
   (if (< arg 0)
       (let ((cursor (mc/furthest-cursor-before-point)))
