80694520a83a2d1d6c7a29b2a8bbdc1ebc61478e
consciencia <consciencia@protonmail.com>
2018-08-14 21:34:50 +0200

    ace jump mode tweak

--- lisp/package-configs/ace-jump-mode__.el
+++ lisp/package-configs/ace-jump-mode__.el
@@ -3,5 +3,5 @@
 
 (setq ace-jump-mode-case-fold t
       ace-jump-mode-move-keys (loop for i from ?a to ?z collect i)
-      ace-jump-word-mode-use-query-char nil
+      ace-jump-word-mode-use-query-char t
       ace-jump-mode-scope 'window)
