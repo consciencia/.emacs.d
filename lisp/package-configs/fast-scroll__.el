92e30c711687b9439b11c42fae679bb50c34ac6a
consciencia <consciencia@protonmail.com>
2019-12-04 22:01:43 +0100

    fixed fast-scroll package lagging

--- lisp/package-configs/fast-scroll__.el
+++ lisp/package-configs/fast-scroll__.el
@@ -1,35 +1,22 @@
-(custom/install-package-when-needed 'fast-scroll)
 (require 'fast-scroll)
-
-
-
-(defun fast-scroll-advice-scroll-functions ()
-  "Wrap as many scrolling functions that we know of in this advice."
-  (interactive)
-  (advice-add #'scroll-up :around #'fast-scroll-run-fn-minimally)
-  (advice-add #'scroll-down :around #'fast-scroll-run-fn-minimally)
-  (advice-add #'scroll-up-command :around #'fast-scroll-run-fn-minimally)
-  (advice-add #'scroll-down-command :around #'fast-scroll-run-fn-minimally)
-  (advice-add #'evil-scroll-up :around #'fast-scroll-run-fn-minimally)
-  (advice-add #'evil-scroll-down :around #'fast-scroll-run-fn-minimally))
-
-(defun fast-scroll-unload-function ()
-  "Remove advice added by `fast-scroll-advice-scroll-functions'.
-Note this function's name implies compatibility with `unload-feature'."
-  (interactive)
-  (advice-remove #'scroll-up #'fast-scroll-run-fn-minimally)
-  (advice-remove #'scroll-down #'fast-scroll-run-fn-minimally)
-  (advice-remove #'scroll-up-command #'fast-scroll-run-fn-minimally)
-  (advice-remove #'scroll-down-command #'fast-scroll-run-fn-minimally)
-  (advice-remove #'evil-scroll-up #'fast-scroll-run-fn-minimally)
-  (advice-remove #'evil-scroll-down #'fast-scroll-run-fn-minimally)
-  nil)
-
 
 
 (fast-scroll-mode 1)
 
-;; TODO:
-;; Get rid of calls to jit-lock-function using
-;;     (add-hook 'fast-scroll-start-hook (lambda () ))
-;;     (add-hook 'fast-scroll-end-hook (lambda () ))
+
+(setq custom/scrolling-flag nil)
+
+(defun custom/started-scrolling ()
+  (setq custom/scrolling-flag t))
+
+(defun custom/stopped-scrolling ()
+  (setq custom/scrolling-flag nil))
+
+(defun custom/around-jit-lock-function (old-fn &rest args)
+  (when (not custom/scrolling-flag)
+    (apply old-fn args)))
+
+
+(advice-add #'jit-lock-function :around 'custom/around-jit-lock-function)
+(add-hook 'fast-scroll-start-hook 'custom/started-scrolling)
+(add-hook 'fast-scroll-end-hook 'custom/stopped-scrolling)
