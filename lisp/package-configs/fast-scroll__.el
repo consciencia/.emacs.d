fb675ab859cad604ba55917140b35e73f3e63bb9
consciencia <consciencia@protonmail.com>
2020-02-06 22:10:34 +0100

    completely reworked 'fast scroll' functionality, a LOT faster and very very short code

--- lisp/package-configs/fast-scroll__.el
+++ lisp/package-configs/fast-scroll__.el
@@ -1,22 +1,31 @@
-(require 'fast-scroll)
-
-
-(fast-scroll-mode 1)
-
+;; A LOT faster solution than here:
+;;     https://github.com/ahungry/fast-scroll
+;; TODO: Report it and try to get it to master,
 
 (setq custom/scrolling-flag nil)
 
-(defun custom/started-scrolling ()
-  (setq custom/scrolling-flag t))
 
-(defun custom/stopped-scrolling ()
+(defun custom/started-scrolling (&rest args)
+  (setq custom/scrolling-flag t))
+(byte-compile 'custom/started-scrolling)
+
+(defun custom/stopped-scrolling (&rest args)
   (setq custom/scrolling-flag nil))
+(byte-compile 'custom/stopped-scrolling)
 
 (defun custom/around-jit-lock-function (old-fn &rest args)
   (when (not custom/scrolling-flag)
     (apply old-fn args)))
+(byte-compile 'custom/around-jit-lock-function)
 
 
-(advice-add #'jit-lock-function :around 'custom/around-jit-lock-function)
-(add-hook 'fast-scroll-start-hook 'custom/started-scrolling)
-(add-hook 'fast-scroll-end-hook 'custom/stopped-scrolling)
+(advice-add #'previous-line :before #'custom/started-scrolling)
+(advice-add #'next-line :before #'custom/started-scrolling)
+(advice-add #'scroll-up :before #'custom/started-scrolling)
+(advice-add #'scroll-down :before #'custom/started-scrolling)
+(advice-add #'scroll-up-command :before #'custom/started-scrolling)
+(advice-add #'scroll-down-command :before #'custom/started-scrolling)
+(advice-add #'evil-scroll-up :before #'custom/started-scrolling)
+(advice-add #'evil-scroll-down :before #'custom/started-scrolling)
+(advice-add #'jit-lock-function :around #'custom/around-jit-lock-function)
+(run-with-idle-timer 0.1 t #'custom/stopped-scrolling)
