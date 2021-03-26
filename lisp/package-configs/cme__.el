50e803b32e0439b7d486db1842ecb2a3d35c9be5
consciencia <consciencia@protonmail.com>
2021-03-26 21:12:01 +0100

    moved CME into separate directory

--- lisp/package-configs/cme__.el
+++ lisp/package-configs/cme__.el
@@ -1,4 +1,9 @@
+(let ((url "https://raw.githubusercontent.com/consciencia/CME/master/cme-install.el")
+      (cme-dir (concat user-emacs-directory "CME")))
+  (when (not (file-directory-p cme-dir))
+    (with-temp-buffer
+      (url-insert-file-contents url)
+      (eval-buffer)))
+  (add-to-list 'load-path cme-dir))
 (require 'cme)
-
-
 (cme-init)
